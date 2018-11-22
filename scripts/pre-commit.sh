eae9cd6f77d8c9211cbcd995eb3e8583dd351e1b
fira swidan <firas@local.com>
2018-11-22 15:21:39 +0200

    Adding echo's to pre-commit script

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-STASH_NAME="pre-commit-$(date +%s)"
-git stash save -q --keep-index $STASH_NAME
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+no_color='\033[0m'
+
+echo -e "\n${yellow}Executing pre-commit hook${no_color}\n"
+
+#STASH_NAME="pre-commit-$(date +%s)"
+#git stash save -q --keep-index $STASH_NAME
 
 ./run_tests.sh
 RESULT=$?
 
-STASHES=$(git stash list)
-if [[ $STASHES == "$STASH_NAME" ]]; then
-  git stash pop -q
+#STASHES=$(git stash list)
+#if [[ $STASHES == "$STASH_NAME" ]]; then
+#  git stash pop -q
+#fi
+
+if [[ $RESULT -ne 0 ]]; then
+	echo -e "\n${red}Git hook failed${no_color}\n"
+	exit 1
 fi
 
-[ $RESULT -ne 0 ] && exit 1
+echo -e "${green}Git hook was SUCCESSFUL!${no_color}\n"
 exit 0
