83d320793a6f37d0e562a1efdd1a9a26dea78767
fira swidan <firas@local.com>
2018-11-23 10:11:03 +0200

    : Gene-Way alerts Adding echo coloring to pre-commit Task-Url:

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -7,16 +7,20 @@ no_color='\033[0m'
 
 echo -e "\n${yellow}Executing pre-commit hook${no_color}\n"
 
+echo `pwd`
+
+
 #STASH_NAME="pre-commit-$(date +%s)"
 #git stash save -q --keep-index $STASH_NAME
 
-./run_tests.sh
+scripts/run_tests.sh
 RESULT=$?
 
 #STASHES=$(git stash list)
 #if [[ $STASHES == "$STASH_NAME" ]]; then
 #  git stash pop -q
 #fi
+
 
 if [[ $RESULT -ne 0 ]]; then
 	echo -e "\n${red}Git hook failed${no_color}\n"
