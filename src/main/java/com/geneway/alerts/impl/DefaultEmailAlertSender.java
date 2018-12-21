3ce879cfb2e6933381354b8810fcdb8dd474fab5
Firas Swidan <frsswdn@gmail.com>
2018-12-21 17:08:47 +0200

    Making class constants non-public

--- src/main/java/com/geneway/alerts/impl/DefaultEmailAlertSender.java
+++ src/main/java/com/geneway/alerts/impl/DefaultEmailAlertSender.java
@@ -11,10 +11,10 @@ public class DefaultEmailAlertSender implements AlertSender {
 
 	//TODO: Password should not be saved in source code.
 	//see: https://stackoverflow.com/questions/12937641/handling-passwords-used-for-auth-in-source-code
-  public static final char[] PASSWORD = "r8B0iR7M".toCharArray();
-  public static final String USER_NAME = "sms.gene.way";
-  public static final String EMAIL_ADDRESS = USER_NAME + "@gmail.com";
-  public static final String HOST = "smtp.gmail.com";
+  static final char[] PASSWORD = "r8B0iR7M".toCharArray();
+  static final String USER_NAME = "sms.gene.way";
+  static final String EMAIL_ADDRESS = USER_NAME + "@gmail.com";
+  static final String HOST = "smtp.gmail.com";
 
   @Override
   public String getUserName() {
